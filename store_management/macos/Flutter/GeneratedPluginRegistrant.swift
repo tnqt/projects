c6fb4fcbd4d483a5dfae26397bfecf7108147413
TN <nxthao0701@gmail.com>
2024-02-13 16:41:42 +0700

    Update store management architecture

--- store_management/macos/Flutter/GeneratedPluginRegistrant.swift
+++ store_management/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,19 @@ import FlutterMacOS
 import Foundation
 
 import firebase_analytics
+import firebase_auth
 import firebase_core
+import firebase_remote_config
+import firebase_storage
+import shared_preferences_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
